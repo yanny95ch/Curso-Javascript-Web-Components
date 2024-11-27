3de28128f8c8c897c3437eb9ad92a1ec86bb4f01
yannych1995 <yannymaly@gmal.com>
2024-11-27 16:35:26 -0500

    MultiContentSlot

---
+++ ContentSlot/my-Element2.js
@@ -0,0 +1,41 @@
+class myElement extends HTMLElement{
+    constructor(){
+        super();
+
+        this.attachShadow({mode:"open"});
+    }
+    getTemplate(){
+        const template = document.createElement('template');
+        template.innerHTML= `
+            <section>
+                <h2>
+                <slot class="title"></slot>
+                </h2>
+                <div>
+                <p>
+                <slot name="parrafo"></slot>
+                </p>
+                </div>
+
+            </section>
+            ${this.getStyle()};
+        `;
+        return template;
+    }
+    getStyle(){
+        return`
+        <style>
+            h2{
+            color: gray
+            }
+        </style
+        `;
+    }
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+    connectedCallback(){
+        this.render();
+    }
+}
+ customElements.define('my-element', myElement);
