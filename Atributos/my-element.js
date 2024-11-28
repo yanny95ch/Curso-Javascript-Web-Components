110ce712b90b7dad153adb22460fd974273076cc
yannych1995 <yannymaly@gmal.com>
2024-11-28 09:52:22 -0500

    Atributos

---
+++ Atributos/my-element.js
@@ -0,0 +1,41 @@
+class myElement extends HTMLElement{
+    constructor(){
+        super();
+
+        this.attachShadow({mode:"open"});
+
+        this.title = this.getAttribute('title');
+        this.parrafo= this.getAttribute('parrafo');
+        this.img= this.getAttribute('img');
+    }
+    getTemplate(){
+        const template = document.createElement('template');
+        template.innerHTML= `
+            <section>
+                <h2> ${this.title} </h2>
+                <div>
+                    <p> ${this.parrafo}</p>
+                <img src=${this.img} / >
+                </div>
+            </section>
+            ${this.getStyle()};
+        `;
+        return template;
+    }
+    getStyle(){
+        return`
+        <style>
+            h2{
+            color: gray
+            }
+        </style
+        `;
+    }
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+    connectedCallback(){
+        this.render();
+    }
+}
+ customElements.define('my-element', myElement);
